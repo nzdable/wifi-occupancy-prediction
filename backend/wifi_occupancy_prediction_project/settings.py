75935b14b555713428222c2db82fd6c31c12bf81
Domadelfin <63452391+Domadelfin@users.noreply.github.com>
2025-10-21 18:57:11 +0800

    feat: enable SSL for database connections by adding ssl_require option

--- backend/wifi_occupancy_prediction_project/settings.py
+++ backend/wifi_occupancy_prediction_project/settings.py
@@ -95,7 +95,12 @@ WSGI_APPLICATION = 'wifi_occupancy_prediction_project.wsgi.application'
 
 DATABASE_URL = os.getenv("DATABASE_URL", "")
 if DATABASE_URL:
-    DATABASES = {"default": dj_database_url.config(default=DATABASE_URL, conn_max_age=600)}
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL, 
+            conn_max_age=600,
+            ssl_require=True
+            )}
 else:
     DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}}
 
